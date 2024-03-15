e0135bad93b52ab5c4e8db6e71e179de74ab3b4d
anik-saha-2001 <aniksahamda@gmail.com>
2024-03-15 11:49:59 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require("dotenv").config({path: "./env"});
 import dotenv from "dotenv"; // SAME SAME BUT DIFF
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
